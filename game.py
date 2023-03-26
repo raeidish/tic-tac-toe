1e739b92e8e7e69092cd2b7e1b67ddd54fa57fef
raeidish <raeidish@gmail.com>
2023-03-26 12:53:32 +0200

    implemented manual game logic

---
+++ game.py
@@ -0,0 +1,53 @@
+import math
+import copy
+
+class TicTacToe:
+    WIN_MASKS = [
+        #horizontal wins
+        [1,1,1,0,0,0,0,0,0],
+        [0,0,0,1,1,1,0,0,0],
+        [0,0,0,0,0,0,1,1,1],
+        #vertical wins
+        [1,0,0,1,0,0,1,0,0],
+        [0,1,0,0,1,0,0,1,0],
+        [0,0,1,0,0,1,0,0,1],
+        #diagonal wins
+        [1,0,0,0,1,0,0,0,1],
+        [0,0,1,0,1,0,1,0,0]
+    ]
+
+    def __init__(self):
+        self.initial_state = ["." for _ in range(0,9)]
+        self.state = copy.deepcopy(self.initial_state)
+        self.move_count = 0
+        
+    def get_move_count(self):
+        return self.move_count
+
+    def get_state(self):
+        return self.state
+    
+    def check_win(self):
+        if(self.move_count == (math.pow(3,2)-1)):
+            return 3
+        
+        for mask in self.WIN_MASKS:
+            if(sum([self.state[i] == "X" if x == 1 else False for i,x in enumerate(mask)]) == 3):
+                return 1
+            if(sum([self.state[i] == "Y" if x == 1 else False for i,x in enumerate(mask)]) == 3):
+                return 2
+
+        return 0
+
+    def make_move(self,index,symbol):
+        #if valid move add in symbol step move_count and return True
+        if(self.state[index] != "X" and self.state[index] != "Y"):
+            self.state[index] = symbol
+            self.move_count += 1
+            return True
+        #if invalid move return false
+        return False
+
+    def reset_board(self):
+        self.move_count = 0
+        self.state = copy.deepcopy(self.initial_state)
--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,70 @@
+from game import TicTacToe
+from time import sleep
+import os
+
+CLEAR_SCREEN = "\x1B[2J"
+MOVE_HOME = "\x1B[H"
+
+PRINT_ESC = lambda code: print(code,end="")
+
+def print_board(state):
+    print("|".join(state[0:3]))
+    print("|".join(state[3:6]))
+    print("|".join(state[6:9]))
+
+def do_move(game):
+    if (game.get_move_count() % 2) == 0:
+        prompt = "X turn choose position [1-9]"
+        symbol = "X"
+    else:
+        prompt = "Y turn choose position [1-9]"
+        symbol = "Y"
+
+    index = input(prompt)
+    index = int(index)-1
+    if(index >= 0 and index <= 8):
+        valid_move = game.make_move(index, symbol)
+        if(valid_move):
+            return
+
+    print("Invalid move try again")
+    do_move(game)
+
+def check_win_or_draw(win_state):
+    match win_state:
+        case 0:
+            return
+        case 1:
+            print("X wins!")
+        case 2:
+            print("Y wins!")
+        case 3:
+            print("It's a Draw")
+
 def main():
-    print("hello")
+    game = TicTacToe()
+    run = True
+    while run:
+        PRINT_ESC(CLEAR_SCREEN)
+        PRINT_ESC(MOVE_HOME)
+
+        print_board(game.get_state())
+
+        win_state = game.check_win()
+        if(win_state > 0):
+            check_win_or_draw(win_state)
+            if(input("press q to quit or any key to reset boad").lower != "q"):
+                game.reset_board()
+            else: 
+                run = False
+        else:
+            do_move(game)
+
+
+
+
 
 if __name__ == "__main__":
+    #for windows 
+    os.system("")
     main()
